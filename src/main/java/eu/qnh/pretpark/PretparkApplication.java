d4d1f86b3208d6b2109ba0f172cca58d1698218c
Raymond Loman <raymond@carpago.nl>
2018-05-23 18:47:18 +0200

    Initial load

---
+++ src/main/java/eu/qnh/pretpark/PretparkApplication.java
@@ -0,0 +1,12 @@
+package eu.qnh.pretpark;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PretparkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PretparkApplication.class, args);
+	}
+}
---
+++ src/test/java/eu/qnh/pretpark/PretparkApplicationTests.java
@@ -0,0 +1,16 @@
+package eu.qnh.pretpark;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class PretparkApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
