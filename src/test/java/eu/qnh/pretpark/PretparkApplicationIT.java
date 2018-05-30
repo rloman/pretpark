15f7e8b7fc9755a1460e84ed384084c17494e542
Raymond Loman <raymond@carpago.nl>
2018-05-30 21:03:52 +0200

    Add the Maven failsafe plugin

--- src/test/java/eu/qnh/pretpark/PretparkApplicationIT.java
+++ src/test/java/eu/qnh/pretpark/PretparkApplicationIT.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class PretparkApplicationTests {
+public class PretparkApplicationIT {
 
 	@Test
 	public void contextLoads() {
